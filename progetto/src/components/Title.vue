14b229ff556cdaa53d3e3bd1b8e91212ed570d4f
Marco Tessitore <marcotexcode@gmail.com>
2021-07-05 14:57:45 +0200

    add esercizio

---
+++ progetto/src/components/Title.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+    <h1>La mia prima app con Vue CLI!</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'Title',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  h1 {
+    color:white;
+    background-color: rgb(70, 70, 70);
+    padding: 40px;
+  }
+
+</style>
