41b1fb5689b2fb0d1d3badd49bbeb25bed66f713
Marco Tessitore <marcotexcode@gmail.com>
2021-07-05 15:31:08 +0200

    bonus

--- progetto/src/App.vue
+++ progetto/src/App.vue
@@ -3,30 +3,40 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Title/>
+    <Hero/>
+
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import Title from './components/Title.vue'
+import Hero from './components/Hero.vue'
+
 
 
 export default {
   name: 'App',
   components: {
   //   HelloWorld
-  Title
+  Title,
+  Hero
   }
 }
 </script>
 
 <style>
+
+body {
+  background-color: rgb(141, 140, 140);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 </style>
---
+++ progetto/src/components/Hero.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="jumbotron">
+
+    <h2>Questo e il mio primo Jumbotron</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'Hero',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .jumbotron{
+    height: 400px;
+    width: 100%;
+    background-image: url(../assets/1.jpg);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+    margin-bottom: 200px;
+  }
+
+  h2{
+    font-size: 50px;
+    text-transform: uppercase;
+    color: rgb(184, 184, 184);
+    padding: 30px;
+    border-radius: 20px;
+    background-color: rgba(0, 0, 0, 0.534);
+  }
+
+</style>
